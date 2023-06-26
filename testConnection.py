e5e3075fc6a4f340696258f61a1c1c34af6c3d81
Faiz Mazhar <faiz@mazhar.net>
2023-06-26 14:16:49 -0400

    first commit

---
+++ testConnection.py
@@ -0,0 +1,20 @@
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper  
+
+class IBapi(EWrapper, EClient):
+     def __init__(self):
+         EClient.__init__(self, self) 
+
+#second argument is socket number found in TWS, 
+#third argument is any positive int
+app = IBapi()
+app.connect('127.0.0.1', 7497, 123)
+app.run()
+
+'''
+#Uncomment this section if unable to connect
+#and to prevent errors on a reconnect
+import time
+time.sleep(2)
+app.disconnect()
+'''
